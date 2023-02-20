b773b05933c4e4aa91d80c506da89936bf8b7b8a
Stanislava <ivanenko.stasy@gmail.com>
2023-02-20 15:15:06 +0300

    new 7

--- sem_7/#34.py
+++ sem_7/#34.py
@@ -1,20 +1,22 @@
 def check_rythms(frase):
     check_values = list("аяуюоеёэиы")
     frase = frase.split()
-    coun = vowels(frase[0],check_values)
+    coun = vowels(frase[0], check_values)
     for i in frase:
-        if vowels(i,check_values)!=coun:
+        if vowels(i, check_values) != coun:
             print("Парам пам-пам")
             exit()
         else:
             continue
     return "Пам парам"
 
-def vowels(string,check_values):
+
+def vowels(string, check_values):
     coun = 0
     for i in list(string):
         if i in check_values:
-            coun+=1
+            coun += 1
     return coun
+
 
 print(check_rythms(input()))
--- sem_7/#36.py
+++ sem_7/#36.py
@@ -1,16 +1,13 @@
-def print_operation_table(operation, num_r=6,num_c=6):
+def print_operation_table(operation, num_r=6, num_c=6):
     arrays = []
-    for i in range(1,num_r+1):
-        ar = [operation(i,y) for y in range(1,num_c+1)]
+    for i in range(1, num_r + 1):
+        ar = [operation(i, y) for y in range(1, num_c + 1)]
         arrays.append(ar)
     return arrays
 
-arrays = print_operation_table(lambda x,y:x**2+2*y)
+
+arrays = print_operation_table(lambda x, y: x ** 2 + 2 * y)
 for i in arrays:
     for j in i:
-        print(' {}'.format(j),end=' ')
+        print(' {}'.format(j), end=' ')
     print()
-
-
-# print("\t  {}  |  {}  |  {}".format(field[0], field[1], field[2]))
-
